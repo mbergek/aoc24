e521d96306b20f9689a9731e37a19df42e298bf0
Martin Bergek <martin@spotwise.com>
2024-12-10 01:11:56 +0100

    December 9

--- 09/d09.rb
+++ 09/d09.rb
@@ -1,31 +1,84 @@
 #!/usr/bin/env ruby
 
-fat = Array.new
+class Record
+  attr_accessor :id, :position, :size
+  def initialize(id, position, size)
+    @id = id
+    @position = position
+    @size = size
+  end
+end
+
+class Space
+  attr_accessor :position, :size
+  def initialize(position, size)
+    @position = position
+    @size = size
+  end
+end
+
+blocks = Array.new
+records = Array.new
+spaces = Array.new
 
 id = 0
+pos = 0
 ARGF.read.chars.map { |c| Integer(c) }.each_slice(2) do |a, b|
-  fat += Array.new(a, id)
-  fat += Array.new(b, nil) unless b.nil?
+  blocks += Array.new(a, id)
+  blocks += Array.new(b, nil) unless b.nil?
+
+  records << Record.new(id, pos, a)
+  spaces << Space.new(pos+a, b) unless b.nil?
   id += 1
+  pos += a
+  pos += b unless b.nil?
 end
 
 head = 0
-tail = fat.size - 1
+tail = blocks.size - 1
 
 while tail > head
-  while ! fat[head].nil? && tail > head
+  while ! blocks[head].nil? && tail > head
     head += 1
-  end  
-  while fat[tail].nil? && tail > head
+  end
+  while blocks[tail].nil? && tail > head
     tail -= 1
   end
-  fat[head] = fat[tail]
-  fat[tail] = nil  
+  blocks[head] = blocks[tail]
+  blocks[tail] = nil
 end
 
 sum = 0
-(0...fat.size).each do |i|
-  sum += i * fat[i] unless fat[i].nil?
+(0...blocks.size).each do |i|
+  sum += i * blocks[i] unless blocks[i].nil?
+end
+
+puts sum
+
+# Part 2
+
+records.reverse_each do |r|
+
+  #puts "Trying to move record: #{r.id}"
+
+  spaces.each do |s|
+    break if s.position > r.position
+
+    if r.size <= s.size
+      #puts "  New position: #{s.position}"
+      r.position = s.position
+      s.position += r.size
+      s.size -= r.size
+      break
+    end
+  end
+end
+
+sum = 0
+records.each do |r|
+  (r.position...r.position+r.size).each do |i|
+    sum += i * r.id
+  end
 end
 
 puts sum
